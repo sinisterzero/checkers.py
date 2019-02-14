bc5578b5e421e44572c767665d71105df551c2ee
csPython <cspython@AD25TF04NGF1J-85.local>
2019-02-14 13:34:36 -0500

    repeated row filling up the screen

--- checkers.py
+++ checkers.py
@@ -1,30 +1,36 @@
 from graphics import*
-winX = 800
-winY = 800
-xran = 8
-lX = 0
-lY = 0
-rX = 100
-rY = 100
-wind = GraphWin("checker", winX, winY)
-wind.setCoords(0, 0, 800, 800)
-#redsquare
+#define square
 def square(lx,ly,rx,ry,red):
     rSqu = Polygon(Point(lx,ly), Point(lx,ry), Point(rx,ry), Point(rx,ly))
     rSqu.setFill(color_rgb(red,0,0))
     rSqu.draw(wind)
     
-#blacksquare
+winX = 800
+winY = 800
+xran = 8
+acolor = 255
+bcolor = 0
 
-bSqu = Polygon(Point(100,0), Point(100,100), Point(200,100), Point(200,0))
+wind = GraphWin("checker", winX, winY)
+wind.setCoords(0, 0, 800, 800)
 
-for i in range(xran):
-    for i in range(xran):
-        square(lX,lY,rX,rY,255)
+lX = 0
+lY = 0
+rX = 100
+rY = 100
+
+for j in range(xran):
+    for i in range(4):
+        square(lX,lY,rX,rY,acolor)
         lX += 100
         rX += 100
-        square(lX,lY,rX,rY,0)
+        square(lX,lY,rX,rY,bcolor)
         lX += 100
         rX += 100
+    lY += 100
+    rY += 100
+    lX = 0
+    rX = 100
+
     
 
