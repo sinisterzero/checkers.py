0f1051387bb0cd00e6a4cd4385cb75634563cb4b
csPython <cspython@AD25TF04NGF1J-85.local>
2019-02-25 10:55:40 -0500

    deleted pointless code for even less line

--- checkers.py
+++ checkers.py
@@ -1,34 +1,33 @@
 from graphics import*
 #window size
 winX = 800
-winY = 800
 check = True
 #color for red and black
 red = 244
 #create window
-wind = GraphWin("checker", winX, winY)
-wind.setCoords(0, 0, 800, 800)
+wind = GraphWin("checker", winX, winX)
+wind.setCoords(0, 0, winX, winX)
 #cords
 ly = 100
-ry = 175
-#define square
+size = 75
+#create board
 for j in range (8):
     lx = 100
-    rx = 175
+    #change color of start square
     if j %2 == 0:
         red = 0
     else:
         red = 244
+    #draw a row
     for i in range (8):
-        rSqu = Polygon(Point(lx,ly), Point(lx,ry), Point(rx,ry), Point(rx,ly))
+        rSqu = Polygon(Point(lx,ly), Point(lx,ly + size), Point(lx + size,ly + size), Point(lx + size,ly))
         rSqu.setFill(color_rgb(red,0,0))
         rSqu.draw(wind)
         lx += 75
-        rx += 75
         if red == 0:
             red = 122
         if red == 244:
             red = 0
         red *= 2
-    ly += 75
-    ry += 75
+    #move up a row
+    ly += size
