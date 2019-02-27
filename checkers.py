8058171de857fafa8d10148c2cc59ca9758ae91e
csPython <cspython@AD25TF04NGF1J-85.local>
2019-02-27 10:12:03 -0500

    less line for changing colors

--- checkers.py
+++ checkers.py
@@ -1,34 +1,20 @@
 from graphics import*
-
 #window size
-print("input the size of the board")
-winX = int(input())
-#color for red and black
+print("Input the size of the board. Recommend less than 100 and more than 10")
+size = int(input())
+swap = {0: 244, 244:0}
+#color
 red = 244
 #create window
-wind = GraphWin("checker", winX, winX)
-wind.setCoords(0, 0, winX, winX)
-#cords
-ly = winX/10
-size = winX/10
+wind = GraphWin("checker", size*10, size*10)
+wind.setCoords(0, 0, size*10, size*10)
 #create board
-for j in range (8):
-    lx = winX/10
+for j in range (8): #move up a row
     #change color of start square
-    if j %2 == 0:
-        red = 0
-    else:
-        red = 244
-    #draw a row
-    for i in range (8):
-        rSqu = Polygon(Point(lx,ly), Point(lx,ly + size), Point(lx + size,ly + size), Point(lx + size,ly))
+    red = swap[red]
+    for i in range (8): #draw a row
+        rSqu = Rectangle(Point(size*(i+1),size*(j+1)), Point(size*(i+2),size*(j+2)))
         rSqu.setFill(color_rgb(red,0,0))
         rSqu.draw(wind)
-        lx += winX/10
-        if red == 0:
-            red = 122
-        if red == 244:
-            red = 0
-        red *= 2
-    #move up a row
-    ly += size
+        #change color
+        red = swap[red]
