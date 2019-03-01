cca2b430a203de40217b0a8c2dcf56bff44b481a
csPython <cspython@AD25TF04NGF1J-85.local>
2019-03-01 09:27:29 -0500

    added custom function

--- checkers.py
+++ checkers.py
@@ -1,8 +1,12 @@
 from graphics import*
+def drawSqu(x, color, window):
+    rSqu = Rectangle(Point(x*(i+1),x*(j+1)), Point(x*(i+2),x*(j+2)))
+    rSqu.setFill(color_rgb(color,0,0))
+    rSqu.draw(window)
 #window size
-print("Input the size of the board. Recommend less than 100 and more than 10")
+print("Input the size of the board. Recommend less than 100 and more than 10, no odd number")
 size = int(input())
-swap = {0: 244, 244:0}
+swap = {0: 244, 244:0} #swap color
 #color
 red = 244
 #create window
@@ -10,11 +14,8 @@ wind = GraphWin("checker", size*10, size*10)
 wind.setCoords(0, 0, size*10, size*10)
 #create board
 for j in range (8): #move up a row
-    #change color of start square
-    red = swap[red]
+    red = swap[red]#swap color for starting square
     for i in range (8): #draw a row
-        rSqu = Rectangle(Point(size*(i+1),size*(j+1)), Point(size*(i+2),size*(j+2)))
-        rSqu.setFill(color_rgb(red,0,0))
-        rSqu.draw(wind)
+        drawSqu(size, red, wind)
         #change color
         red = swap[red]
